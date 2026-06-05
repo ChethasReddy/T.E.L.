2df8aa0bdcc21f4472f2c30f445fb34f1bcf3287
ChethasReddy <134313385+ChethasReddy@users.noreply.github.com>
2026-06-05 17:22:39 -0400

    saving domain selection feature (#3)

---
+++ components/BackgroundBlobs.tsx
@@ -0,0 +1,9 @@
+export default function BackgroundBlobs() {
+  return (
+    <>
+      <div className="blob blob-indigo w-[600px] h-[600px] -top-40 -left-40" />
+      <div className="blob blob-violet w-[500px] h-[500px] top-40 -right-32" />
+      <div className="blob blob-pink w-[400px] h-[400px] bottom-0 left-1/3 opacity-30" />
+    </>
+  )
+}
